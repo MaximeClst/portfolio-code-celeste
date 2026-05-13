ee2d21e0e2b5f7eb170810e1754a22b464c8cbde
Maxime Celeste <maxime.celeste1221@gmail.com>
2026-05-13 09:00:13 +0400

    replacement comparatif

--- app/page.tsx
+++ app/page.tsx
@@ -24,10 +24,10 @@ export default function Home() {
         <Header />
         <Hero />
         <HowItWorks />
+        <Comparison />
         <Work />
         <Testimonial />
         <Feature />
-        <Comparison />
         <FinalCTA />
         <Footer />
       </div>
